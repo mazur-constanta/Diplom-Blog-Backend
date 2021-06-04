84b4d782aeb2f0c67774c3922a9559009327fef8
Constance <constancegerman@gmail.com>
2021-06-04 15:42:51 +0300

    Modified space in methods

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
-import path from 'path';
 
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 const withDB = async (operations) => {
     try {
@@ -21,18 +20,22 @@ const withDB = async (operations) => {
     }
 }
 
-// app.get('api/articles', async (req, res) => {
-//     await withDB(async (db) => {
-//         const articleInfo = await db.collection('articles').find();
-//         res.status(200).json(articleInfo);
-//     });
-// })
+app.get('api/articles/', async (req, res) => {
+
+    await withDB(async (db) => {
+        const articleInfo = await db.collection('articles').find();
+
+        res.status(200).json(articleInfo);
+    });
+})
 
 app.get('/api/articles/:name', async (req, res) => {
+
     const articleName = req.params.name;
 
     await withDB(async db => {
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
+
         res.status(200).json(articleInfo);
     });
 });
@@ -47,31 +50,37 @@ app.get('/api/articles/:name', async (req, res) => {
 // })
 
 app.post('/api/articles/:name/upvote', async (req, res) => {
+
     const articleName = req.params.name;
 
     await withDB(async db => {
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
+
         await db.collection('articles').updateOne({ name: articleName }, {
             '$set': {
                 upvotes: articleInfo.upvotes + 1,
             }
         });
+
         const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
         res.status(200).json(updatedArticleInfo);
     });
 });
 
 app.post('/api/articles/:name/add-comment', async (req, res) => {
+
     const articleName = req.params.name;
     const newComment  = req.body.comment;
 
     await withDB(async (db) => {
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
+
         await db.collection('articles').updateOne({ name: articleName }, {
             '$set': {
                 comments: articleInfo.comments.concat(newComment),
             }
         });
+
         const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
         res.status(200).json(updatedArticleInfo);
     });
